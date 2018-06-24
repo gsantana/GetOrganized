1a97f7dcb9345ce185c215982a1c0ecab5b26d74
Gustavo <guansa@gmail.com>
2018-06-24 12:44:25 -0300

    Inicial commit

---
+++ GetOrganized/Controllers/TodoController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GetOrganized.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetOrganized.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: Todo
+        public ActionResult Index()
+        {
+            return View(Todo.ThingsToBoDone);
+        }
+
+        // GET: Todo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Todo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Todo todo)
+        {
+            try
+            {
+                Todo.ThingsToBoDone.Add(todo);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Todo/Edit/5
+        public ActionResult Edit(string title)
+        {
+            return View(Todo.ThingsToBoDone.Find(x=> x.Title == title));
+        }
+
+        // POST: Todo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string oldTitle, Todo newTodo)
+        {
+            try
+            {
+                Todo.ThingsToBoDone.RemoveAll(x => x.Title == oldTitle);
+                Todo.ThingsToBoDone.Add(newTodo);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Todo/Delete/5
+        public ActionResult Delete(string title)
+        {
+            Todo.ThingsToBoDone.RemoveAll(x => x.Title == title);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Todo/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GetOrganized/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetOrganized.Models
+{
+    public class Todo
+    {
+        public static List<Todo> ThingsToBoDone = new List<Todo>() {
+            new Todo() { Title = "Get Milk", Completed = false},
+            new Todo() { Title = "Bringg Home Bacon", Completed = true}
+        };
+
+        public bool Completed { get; set; }
+        public string Title { get; set; }
+    }
+}
